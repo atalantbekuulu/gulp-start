9bce098aac8a4c1035d104886a9facb10254d7e1
atalantbekuulu <azamattalantbekov336@gmail.com>
2024-05-12 17:47:51 +0600

    добавление кнопкам вызова модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 const navBar = document.querySelector(".main-nav");
 const BtnCloseNavBar = document.querySelector(".main-nav__close");
 const btnOpenNavbar = document.querySelector(".header__burger");
+const AllBntAddProduct = document.querySelectorAll('.product-exclusive__button,.product__button')
+const modal = document.querySelector('#modal_cart')
+const modalBtn = document.querySelectorAll('#modal_cart > button')
 
 const removeClassNavbar = () => {
   navBar.classList.remove("main-nav--showed");
@@ -13,3 +16,18 @@ const addclassNavbar = () => {
 };
 
 btnOpenNavbar.addEventListener("click", addclassNavbar);
+
+AllBntAddProduct.forEach((button)=>{
+   button.addEventListener('click',()=>{
+    modal.classList.remove('modal--hidden')
+   })
+})
+
+const clickBtnModal = (button) => {
+  modal.classList.add('modal--hidden')
+  button.removeEventListener('click',clickBtnModal);
+}
+
+modalBtn.forEach(button=>{
+  button.addEventListener('click',clickBtnModal);
+})
