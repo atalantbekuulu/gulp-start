bf83893507b20189a5366649fff132b23aedcbf0
atalantbekuulu <azamattalantbekov336@gmail.com>
2024-05-13 20:30:41 +0600

    исправление наименовании и разделение модального окна. Добавил условный оператор if

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,7 @@
 const navBar = document.querySelector(".main-nav");
 const BtnCloseNavBar = document.querySelector(".main-nav__close");
 const btnOpenNavbar = document.querySelector(".header__burger");
-const AllBntAddProduct = document.querySelectorAll('.product-exclusive__button,.product__button')
-const modal = document.querySelector('#modal_cart')
-const modalBtn = document.querySelectorAll('#modal_cart > button')
+
 
 const removeClassNavbar = () => {
   navBar.classList.remove("main-nav--showed");
@@ -16,18 +14,3 @@ const addclassNavbar = () => {
 };
 
 btnOpenNavbar.addEventListener("click", addclassNavbar);
-
-AllBntAddProduct.forEach((button)=>{
-   button.addEventListener('click',()=>{
-    modal.classList.remove('modal--hidden')
-   })
-})
-
-const clickBtnModal = (button) => {
-  modal.classList.add('modal--hidden')
-  button.removeEventListener('click',clickBtnModal);
-}
-
-modalBtn.forEach(button=>{
-  button.addEventListener('click',()=>{clickBtnModal(button)});
-})
